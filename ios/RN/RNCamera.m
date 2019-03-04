3d93c8bc2200ea6dde84b49a568a65e29631462c
Luis Villalobos <alfredovillalobo@gmail.com>
2019-03-04 17:42:15 -0500

    Fix zoom behavior for iOS

--- ios/RN/RNCamera.m
+++ ios/RN/RNCamera.m
@@ -243,7 +243,7 @@ static NSDictionary *defaultFaceDetectorOptions = nil;
         return;
     }
 
-    device.videoZoomFactor = (device.activeFormat.videoMaxZoomFactor - 1.0) * self.zoom + 1.0;
+    device.videoZoomFactor = MIN(MAX(self.zoom, 1), device.activeFormat.videoMaxZoomFactor);
 
     [device unlockForConfiguration];
 }
