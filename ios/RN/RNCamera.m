06d1287f2c20c637fa04ab5797dcb1c5a700d55c
Luis Villalobos <alfredovillalobo@gmail.com>
2019-03-08 10:23:15 -0500

    Fix zoom behavior for ns-pos

--- ios/RN/RNCamera.m
+++ ios/RN/RNCamera.m
@@ -294,10 +294,8 @@ static NSDictionary *defaultFaceDetectorOptions = nil;
         }
         return;
     }
-
+    
     device.videoZoomFactor = MIN(MAX(self.zoom, 1), device.activeFormat.videoMaxZoomFactor);
-
-    // device.videoZoomFactor = (device.activeFormat.videoMaxZoomFactor - 1.0) * self.zoom + 1.0;
 
     [device unlockForConfiguration];
 }
